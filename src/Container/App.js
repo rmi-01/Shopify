daf301df60f935a7fc2347b4038b87346337b9ea
ebuildSlayer <mohammadibrahim.ibrahim9@gmail.com>
2020-04-01 16:47:37 +0500

    Adding files

---
+++ src/Container/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import NavBar from '../Components/NavBar/NavBar';
+
+class App extends React.Component{
+
+	render(){
+		return (
+		   	<div>
+		    	<NavBar/>
+		    </div>
+		);
+	}	
+}
+
+export default App;
